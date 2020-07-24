a5545e146223e196b4ba9cd172a053329379fb78
Sree Pittala <ssapittala1@sheffield.ac.uk>
2020-07-24 08:55:59 +0100

    UC-2 Completed

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+####### First Name Validation
+
 read -p "Enter first name: " firstName
 pattern1="^[A-Z]{1}[a-z]{2}$"
 
@@ -9,3 +11,17 @@ then
 else
 	echo "First Name is not valid"
 fi
+
+####### Last Name Validation
+
+read -p "Enter last name: " lastName
+pattern2="^[A-Z]{1}[a-z]{2}$"
+
+if [[ $lastName =~ $pattern2 ]]
+then
+	echo "Last Name is valid"
+else
+	echo "Last Name is not valid"
+fi
+
+
